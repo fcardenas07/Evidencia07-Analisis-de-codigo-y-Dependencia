979c5b886481099879e58897175a8fc1906bb43f
fcardenas07 <f.cardenasreyes.94@gmail.com>
2023-05-08 22:56:35 -0400

    Código Visual Paradigm

---
+++ src/main/java/Calculadora_VP.java
@@ -0,0 +1,28 @@
+public class Calculadora_VP {
+    private int n1;
+    private int n2;
+
+    public Calculadora_VP() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Calculadora_VP(int num1, int num2) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int sumar() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int multiplicar() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setN1(int num1) {
+        this.n1 = num1;
+    }
+
+    public void setN2(int num2) {
+        this.n2 = num2;
+    }
+}
---
+++ src/main/java/CarroCompra_VP.java
@@ -0,0 +1,19 @@
+public class CarroCompra_VP {
+    private int[][] productos;
+
+    public CarroCompra_VP() {
+        throw new UnsupportedOperationException();
+    }
+
+    private int calcularTotal() {
+        throw new UnsupportedOperationException();
+    }
+
+    private int subTotal(int cant, int precio) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void mostrarTotal() {
+        throw new UnsupportedOperationException();
+    }
+}
