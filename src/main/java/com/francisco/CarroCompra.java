871b65df77d7733c4552d79cccb8d41d5e6a2fb9
fcardenas07 <f.cardenasreyes.94@gmail.com>
2023-05-08 22:57:01 -0400

    Carro y Calculadora

---
+++ src/main/java/com/francisco/CarroCompra.java
@@ -0,0 +1,30 @@
+package com.francisco;
+
+public class CarroCompra {
+    private int[][] productos = new int[2][5];
+
+    public CarroCompra() {
+        for (int i = 0; i < 5; i++) {
+            productos[0][i] = 1;
+            productos[1][i] = 1000;
+        }
+    }
+
+    private int calcularTotal() {
+        int total = 0;
+
+        for (int i = 0; i < 5; i++) {
+            total += subTotal(productos[0][i], productos[1][i]);
+        }
+        return total;
+    }
+
+    private int subTotal(int cantidad, int precio) {
+        Calculadora calculadora = new Calculadora(cantidad, precio);
+        return calculadora.multiplicar();
+    }
+
+    public void mostrarTotal() {
+        System.out.println("El total de la compra es: " + this.calcularTotal());
+    }
+}
